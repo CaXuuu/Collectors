d738ea0a5a7262ce8b1428879b7f3cb1109f3a0a
maciek <maciekmalyszek@gmail.com>
2025-08-28 20:38:52 +0200

    toSet

--- src/toListSetMap.java
+++ src/toListSetMap.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
+
+import static java.util.stream.Collectors.*;
 
 public class toListSetMap {
     public static void run(){
@@ -11,10 +13,23 @@ public class toListSetMap {
         teams.add("Slovakia");
         teams.add("Spain");
         teams.add("Sweden");
+        teams.add("Sweden");
 
         Collection<String> teamsStartingWithS = teams.stream()
                 .filter(team -> team.startsWith("S"))
-                .collect(Collectors.toList());
+                .collect(toList());
         System.out.println(teamsStartingWithS);
+
+
+
+        Set<String> setOfTeamsStartingWithS = teams.stream()
+                .filter(team -> team.startsWith("S"))
+                .collect(toSet()); // nie pojawiaja sie te same druzyny
+        System.out.println(setOfTeamsStartingWithS);
+    }
+    public static void main(String[] args) {
+
+        toListSetMap.run();
+
     }
 }
